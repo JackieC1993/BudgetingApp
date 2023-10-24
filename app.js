84126e1c2349785c89789ea718c548c15e094e40
JackieC1993 <jackiecheung@pursuit.org>
2023-10-23 22:19:54 -0400

    added server js, added, app.js and the other requirements

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const transactionsController = require('./controllers/transactionsController');
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/transactions', transactionsController);
+
+module.exports = app;
+
