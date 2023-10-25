d902769135f0039341d9112ff8c29aea72b4e512
Jackie Cheung <131386755+JackieC1993@users.noreply.github.com>
2023-10-25 00:39:29 -0400

    Delete budgetFrontend/vite-project directory

--- budgetFrontend/vite-project/src/components/AddTransactionForm.js
+++
@@ -1,9 +0,0 @@
-function addTransactionForm(){
-    return(
-        <div>
-            This is the add transaction form.
-        </div>
-    )
-}
-
-export default addTransactionForm;
--- budgetFrontend/vite-project/src/components/TransactionCard.js
+++
@@ -1,9 +0,0 @@
-function TransactionCard(){
-    return(
-        <div>
-            This is the transaction card.
-        </div>
-    )
-}
-
-export default TransactionCard;
